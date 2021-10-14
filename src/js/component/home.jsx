d1479c4039b49ebc2cdd22f95fc5937651027a89
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-10-14 19:14:43 +0000

    navbar create, not working on preview

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
 
-export default Home;
+}
+const Navbar = () => {
+	<div className="navbar navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">Start Bootstrap</a>
+		<ul className="navbar-nav mr-auto">
+			<li className="nav-item active">
+				<a className="nav-link" href="#">Home <span classNames="sr-only">(current)</span></a>
+			</li>,
+			<li className="nav-item">
+				<a className="nav-link" href="#">Features</a>
+			</li>,
+			<li className="nav-item">
+				<a className="nav-link" href="#">Pricing</a>
+			</li>
+		</ul>
+	</div>
+}
+
+const Junmotron = () => {
+
+}
+
+const Card = () => {
+
+}
+
+export default Home; Navbar;
