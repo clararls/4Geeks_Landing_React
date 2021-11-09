32c7b8a96a375731800b87addb7dcc445250cefc
Clara Rojano León-Salas <clararojanols@gmail.com>
2021-11-09 12:24:29 +0000

    Fixed Navbar and Footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="row">
-			<p className="navbar navbar-dark bg-dark d-flex justify-content-center sticky-bottom text-white mb-0 p-3">
+		<div className="row fixed-bottom">
+			<p className="navbar navbar-dark bg-dark d-flex justify-content-center text-white mb-0 p-3">
 				Copyright @ 2021 Your Webssite
 			</p>
 		</div>
